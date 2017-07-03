be4c8f860072b61dfa9d959e066d30dc219f1300
Raul Rodriguez <raul1.rodriguez@noexternalmail.hsbc.com>
2017-07-03 18:18:28 +0100

    Base dropwizard applications for the frontend and backend applications.

---
+++ web-application/front-end/src/main/java/com/github/raulra08/frontend/HelloWorldConfiguration.java
@@ -0,0 +1,40 @@
+package com.github.raulra08.frontend;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import io.dropwizard.Configuration;
+
+public class HelloWorldConfiguration extends Configuration {
+
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String logging;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String defaultName) {
+        this.defaultName = defaultName;
+    }
+
+}

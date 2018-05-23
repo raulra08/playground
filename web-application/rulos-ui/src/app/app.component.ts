6e314ad660a4e26ef9a99135b16d5371be8fb1e1
Raul Rodriguez <rulo@Rauls-MacBook-Pro.local>
2018-05-23 20:04:07 +0100

    rulos-ui

--- web-application/rulos-ui/src/app/app.component.ts
+++ web-application/rulos-ui/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
-
+  title = 'app';
 }
---
+++ web-application/rulos-ui/src/app/body/body.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'body-cmp',
+    templateUrl: './body.component.html'
+})
+export class BodyComponent {
+
+}
